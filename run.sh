5ae4ba10dcc62a7931a7d67f81099dc03430d93d
周睿 <zrufo747@outlook.com>
2025-06-30 17:00:02 +0800

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-make  -C /home/zhourui/arceos/arceos-plt-dyn A=$(pwd) LOG=debug LD_SCRIPT=link.x PLAT_CONFIG=/home/zhourui/arceos/axplat_crates/platforms/axplat-aarch64-dyn/axconfig.toml SMP=2 MYPLAT=axplat-aarch64-dyn run
+make  -C /home/zhourui/arceos/arceos-plt-dyn A=$(pwd) LOG=debug LD_SCRIPT=link.x PLAT_CONFIG=/home/zhourui/arceos/axplat_crates/platforms/axplat-aarch64-dyn/axconfig.toml  FEATURES=driver-dyn MYPLAT=axplat-aarch64-dyn run
+# make  -C /home/zhourui/arceos/arceos-plt-dyn A=$(pwd) LOG=debug LD_SCRIPT=link.x SMP=2 FEATURES=driver-dyn MYPLAT=axplat-aarch64-dyn run
