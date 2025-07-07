00d6fdcf2b319feecc7927643718286cfe60ae5e
周睿 <zrufo747@outlook.com>
2025-07-07 10:03:57 +0800

    fix merge

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-make -C /home/zhourui/arceos/arceos-plt-dyn A=$(pwd) LOG=info LD_SCRIPT=link.x PLAT_CONFIG=/home/zhourui/arceos/axplat_crates/platforms/axplat-aarch64-dyn/axconfig.toml  FEATURES=driver-dyn MYPLAT=axplat-aarch64-dyn run
-# make  -C /home/zhourui/arceos/arceos-plt-dyn A=$(pwd) LOG=debug LD_SCRIPT=link.x SMP=2 FEATURES=driver-dyn MYPLAT=axplat-aarch64-dyn run
+make -C /home/zhourui/arceos/arceos-plt-dyn A=$(pwd) LOG=info LD_SCRIPT=link.x FEATURES=driver-dyn MYPLAT=axplat-aarch64-dyn run
