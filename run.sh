0fb4ea49f38566f94188277c157527adb4b86c8c
周睿 <zrufo747@outlook.com>
2025-07-08 17:32:01 +0800

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 export CARGO_NET_OFFLINE=true
-make -C /home/zhourui/arceos/arceos-plt-dyn A=$(pwd) LOG=debug LD_SCRIPT=link.x FEATURES=driver-dyn SMP=2 MYPLAT=axplat-aarch64-dyn run
+make -C /home/zhourui/arceos/arceos-plt-dyn A=$(pwd) LOG=info LD_SCRIPT=link.x FEATURES=driver-dyn SMP=2 MYPLAT=axplat-aarch64-dyn run
